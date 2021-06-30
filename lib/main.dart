8c9f9e0466b8f11535ae2c6319f77af317415727
Nada <Nada@DESKTOP-6FOVJ24>
2021-07-01 00:55:54 +0300

    shared_Pref

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:shop01_app/modules/on_boarding_screen.dart';
+import 'package:shop01_app/shared/network/local/cash_helper.dart';
 import 'package:shop01_app/shared/network/remote/dio_helper.dart';
 
 void main() {
-   DioHelper.init();
+  WidgetsFlutterBinding();
+  DioHelper.init();
+  CashHelper.init();
+
   runApp(ShopApp());
 }
-  
-  
-
 
 class ShopApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner:false ,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.deepOrange,
           floatingActionButtonTheme: FloatingActionButtonThemeData(
